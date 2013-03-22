2cc680f83edbf288a707d7aef8c0b78f182eb03c
Maxim <BVMaks@gmail.com>
2013-03-22 20:08:54 +0000

    Activation class and implementation of it`s methods were added. Symbol class was also added to /parser/ast.

---
+++ sources/Frontend/interpreter/activation.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file:activation.hpp
+ * Activation class.
+ */
+/**
+ * Copyright 2012 MIPT-COMPILER team
+ */
+
+#pragma once
+
+#include "../parser/ast.hpp"
+#include <map>
+#include <boost/shared_ptr.hpp>
+
+namespace interpreter
+{
+    using parser::ast::Nodep;
+    using parser::ast::Symbol;
+    using parser::ast::SymbolIdType;
+
+    class Activation : public std::map<SymbolIdType, Nodep>
+    {
+    public:
+        Activation( Activation* parent);
+        Nodep get( SymbolIdType symbol_id);
+        void add( Symbol& symbol);
+        Activation* getParentPtr();
+        
+        ~Activation();
+
+    private:
+        boost::shared_ptr<Activation> parent_activation_ptr;
+    };
+}
