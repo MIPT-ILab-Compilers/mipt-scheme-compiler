6398ac61e2cfd085f6d2d712533ad9fba39e4d14
Vadim.Macishin <Vadim.Macishin@frtk.ru>
2013-10-11 16:18:39 +0000

    Vadim stack

--- exercises/ex1/stack.cpp
+++ exercises/ex1/stack.cpp
@@ -10,5 +10,13 @@
  */
 void testStack()
 {
-
+	Stack my_stack;
+	for ( int i = 0; i < 5; i++ )
+		my_stack.push(i);
+	for ( int i = 0; i < 5; i++ )
+		cout << my_stack.pop() << endl;
+	cout << my_stack.pop() << endl;
+	my_stack.push(7);
+	int a = my_stack.pop();
+	cout << a << endl;
 }
