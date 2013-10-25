4ae4229be65a7d5785dc7e7b6d95771405321618
Vadim.Macishin@frtk.ru <Vadim.Macishin@frtk.ru@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2013-10-25 14:47:44 +0000

    stack

--- branches/playground/exercises/ex1/main.cpp
+++ branches/playground/exercises/ex1/main.cpp
@@ -4,15 +4,16 @@
  * Test cpp file for the exercise 1
  * @author Boris Shurygin
  */
-#include "list.h"
-
+#include<iostream>
+#include "misc.h"
+using namespace std;
 void testStack();
 
 
 
 int main( int argc, char** argv)
 {
-	testList();
+//	testList();
 	testStack();
 	cout << "Exercise 1 test complete in " << BUILD_MODE << " mode!" << endl;
 	return 0;
--- branches/playground/exercises/ex1/stack.cpp
+++ branches/playground/exercises/ex1/stack.cpp
@@ -4,19 +4,29 @@
  * @author Boris Shurygin
  */
 #include "stack.h"
-
 /**
  * Unit testing of Stack
  */
+
 void testStack()
 {
-	Stack my_stack;
+	Stack <int> my_stack(3);
 	for ( int i = 0; i < 5; i++ )
 		my_stack.push(i);
+//	cout << my_stack;// << endl;
 	for ( int i = 0; i < 5; i++ )
 		cout << my_stack.pop() << endl;
 	cout << my_stack.pop() << endl;
 	my_stack.push(7);
 	int a = my_stack.pop();
+	Stack <char*> str;
+	str.push("Vadim\n");
+	str.push("name is ");
+	str.push("Hi, my ");
+	char * s;
+//	cout << str << endl;
+	while ( ( s = str.pop() ) != 0 )
+		cout << s;	
+	
 	cout << a << endl;
 }
