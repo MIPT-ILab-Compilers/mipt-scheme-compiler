70eb202acaf842bccc17f352e0e419cad7cab5a0
Artem.Rudoj@frtk.ru <Artem.Rudoj@frtk.ru@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2013-11-14 21:43:52 +0000

    Add file.

--- branches/playground/exercises/ex1/list.cpp
+++ branches/playground/exercises/ex1/list.cpp
@@ -20,16 +20,12 @@ template <class T> Dlist<T>::Dlist()
 	prev = this;
 }
 
-
-
 template <class T> Dlist<T>::Dlist( T value)
 {
 	data = value;
 	next = this;
 	prev = this;
 }
-
-
 
 template <class T> Dlist<T>::Dlist( T value, Dlist<T>* current_elem)   
 {   
@@ -40,15 +36,11 @@ template <class T> Dlist<T>::Dlist( T value, Dlist<T>* current_elem)
 	this->data = value;
 }	
 
-
-
 template <class T> Dlist<T>::~Dlist()
 {
 	next->prev = prev;
 	prev->next = next;
 }	
-
-
 
 template <class T> Dlist<T>* Dlist<T>::add( T value, Dlist<T> *current_elem)   //Added object after "*current_elem" with data          
 {                                                                              // data with the value "value"
@@ -72,28 +64,24 @@ template <class T> Dlist<T>* Dlist<T>::add( T value, Dlist<T> *current_elem)   /
 	    }
 }
 
-
-    
-template <class T> Dlist<T>* Dlist<T>::add( Dlist<T> *ins_elem , Dlist<T> *current_elem) //Added "ins_elem" after "*current_elem"
+template <class T> Dlist<T>* Dlist<T>::add( Dlist<T> *ins_elem) 
 {
-	if ( current_elem == NULL )                       
+	if ( this == NULL )                       
 	{   
 		ins_elem->prev = ins_elem;
 		ins_elem->next = ins_elem;
     	LOG(cout << "Added" << endl);
-	    return ins_elem;
-    }else 
+    	return ins_elem;
+	}else 
 	{
-		(current_elem->next)->prev = ins_elem;
-		ins_elem->next = current_elem->next;
-		current_elem->next = ins_elem;
-		ins_elem->prev = current_elem;
+		(this->next)->prev = ins_elem;
+		ins_elem->next = this->next;
+		this->next = ins_elem;
+		ins_elem->prev = this;
 		return ins_elem;
 	}
 }		
 			     
-
-
 template <class T> void Dlist<T>::print( Dlist<T> *current_elem)
 {   
 	Dlist<T> *temp;
@@ -113,8 +101,6 @@ template <class T> void Dlist<T>::print( Dlist<T> *current_elem)
     return;
 }	
 
-   	
- 
 template <class T> Dlist<T>* Dlist<T>::find( T value, Dlist<T> *current_elem)
 {
 	 Dlist<T> *temp;
@@ -133,24 +119,77 @@ template <class T> Dlist<T>* Dlist<T>::find( T value, Dlist<T> *current_elem)
 	return current_elem;	 
 }
 
+template <class T> DlistIter<T> Dlist<T>::myIter()
+{   
+	DlistIter<T> *iter = new DlistIter<T>;
+	iter->current = this;
+	return *iter;
+}
 
+template <class T> DlistIter<T> DlistIter<T>::operator++ ()             //++it
+{   
+	this->current = (this->current)->next;
+	return *this;
+}
+	
+template <class T> DlistIter<T> DlistIter<T>::operator++ ( int)         //it++
+{   
+	DlistIter temp = *this;
+	++(*this);
+	return temp;
+}	
 
-void testList()
-{
-	Dlist<int> dlist1(5),dlist2(7);
-    Dlist<int> *current_elem;
-	current_elem = dlist1.add( 1 , &dlist1);
-    dlist1.print(&dlist1);
-    delete current_elem;
-	current_elem = dlist1.add( &dlist2 , &dlist1);
-    dlist1.add( 3 , current_elem);
-    dlist1.print(&dlist1);
-    Dlist<double> Dlist_d1(3.14);
-    Dlist<double>  *Dlist_d2;
-    Dlist_d2 = &Dlist_d1;
-    Dlist_d2->add(2.73, Dlist_d2);
-    Dlist_d2->add(1.38, Dlist_d2);
-    Dlist_d2->print(Dlist_d2);
+template <class T> DlistIter<T> DlistIter<T>::operator-- ()             //--it
+{   
+	this->current = (this->current)->prev;
+	return *this;
+}
+
+template <class T> DlistIter<T> DlistIter<T>::operator-- ( int)         //it--
+{   
+	DlistIter  temp = *this;
+	--(*this);
+	return temp;
+}	
     
-   
+template <class T> T DlistIter<T> ::operator*()
+{
+	return (this->current)->data;
+}
+
+template <class T> void DlistIter<T>::operator= ( DlistIter<T> iter) 
+{   
+	if ( this == &iter)
+	    return;
+	this->current = new Dlist<T>;   
+	this->current = iter.current;
+}	
+
+template <class T> bool DlistIter<T>::operator== ( DlistIter<T> iter)
+{
+	return ( this->current == iter.current);
+}
+	
+void testList()
+{   
+	DlistIter<int> iter1, iter2;
+	Dlist<int> dlist1(5),dlist(7);
+	iter1 = dlist1.myIter();
+	dlist1.add( 1 , &dlist1);      // 5 1 7 5 7 5
+	dlist1.add(&dlist);
+	iter2 = iter1;
+	if ( iter1 == iter2 ) 
+	    cout << "iter 1 is equal to iter 2" << endl;
+	else
+	    cout << "iter 1 is not equal to iter 2" << endl; 
+	iter1++;
+	if ( iter1 == iter2 ) 
+	    cout << "iter 1 is equal to iter 2" << endl;
+	else
+	    cout << "iter 1 is not equal to iter 2" << endl;
+	iter1 = iter2++;    
+	cout << "iter 1 is " << *iter1 << endl;
+    cout << "iter 2 is " << *iter2 << endl;
+    ++iter2;
+    cout << "iter 2 is "<< *iter2 << endl;
 }    
