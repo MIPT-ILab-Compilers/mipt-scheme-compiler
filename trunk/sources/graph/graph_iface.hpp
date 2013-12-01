38df145b85aade76052d5112134f0a1bf605e60b
rob.khasanov@gmail.com <rob.khasanov@gmail.com@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2013-12-01 23:20:55 +0000

    Graph structure refactoring (2)

---
+++ trunk/sources/graph/graph_iface.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file:graph_iface.hpp
+ * Interface of graph library
+ */
+/**
+ * Copyright (C) 2013  MIPT Scheme Compiler team
+ */
+
+#pragma once
+
+#include "predecls.hpp"
+
+#include "graph.hpp"
+#include "node.hpp"
+#include "edge.hpp"
