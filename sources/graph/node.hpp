420340053e02d0330ceb3c724335c09ed8a28878
sash.tsvet <sash.tsvet@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2014-03-24 16:53:56 +0000

    Graph: some functions added, fields which gonna be private set

--- sources/graph/node.hpp
+++ sources/graph/node.hpp
@@ -12,11 +12,19 @@
 
 class Node
 {
-public:
+public:									//to be private
 	int data;
 	Node *next, *prev;
 	Edge *pred_edge, *succ_edge;		// Predecessors - edges "to" here
 										// Successors - edges "from" here
+public:
 	Node ( int data);
 	~Node () {};
+	Edge* getPred()
+	{
+		return pred_edge;
+	};
+	Edge* getSucc(){
+		return succ_edge;
+	}
 };
