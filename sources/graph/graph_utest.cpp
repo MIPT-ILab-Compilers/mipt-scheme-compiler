48d4d0c122a6edcf68539519596484f91b6f4506
sash.tsvet <sash.tsvet@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2014-04-05 14:12:06 +0000

    Graph: bug 7 fixed

--- sources/graph/graph_utest.cpp
+++ sources/graph/graph_utest.cpp
@@ -27,8 +27,8 @@ BOOST_AUTO_TEST_CASE( graph_basic)
 	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, null);
 
 	g1.addNode();
-	BOOST_CHECK_EQUAL( g1.first_node->data, 1);
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
+	BOOST_CHECK_EQUAL( g1.last_node->data, 1);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
 	BOOST_CHECK_EQUAL( g1.first_edge, null);
 	BOOST_CHECK_EQUAL( g1.last_edge, null);
 	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
@@ -42,8 +42,8 @@ BOOST_AUTO_TEST_CASE( graph_basic)
 
 
 	g1.addEdge( g1.first_node, g1.last_node );
-	BOOST_CHECK_EQUAL( g1.first_node->data, 1);
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
+	BOOST_CHECK_EQUAL( g1.last_node->data, 1);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
 	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
 	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
 	BOOST_CHECK_EQUAL( g1.last_node->next, null);
@@ -57,60 +57,60 @@ BOOST_AUTO_TEST_CASE( graph_basic)
 	BOOST_CHECK_EQUAL( g1.first_edge->prev, null);
 
 	g1.addEdge( g1.last_node, g1.last_node );
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
-	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
-	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
-	BOOST_CHECK_EQUAL( g1.last_node->next, null);
-	BOOST_CHECK_EQUAL( g1.last_node->prev, g1.first_node);
-	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, null);
-	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, g1.last_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.first_edge->next, g1.last_edge);
-	BOOST_CHECK_EQUAL( g1.last_edge->prev, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.first_edge->prev, null);
-
-
-	g1.rmEdge( g1.first_edge );
-	BOOST_CHECK_EQUAL( g1.first_node->data, 1);
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
 	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
 	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
 	BOOST_CHECK_EQUAL( g1.last_node->next, null);
 	BOOST_CHECK_EQUAL( g1.last_node->prev, g1.first_node);
 	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, null);
 	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.first_edge->next, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.last_edge->prev, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.first_edge->prev, null);
+
+
+	g1.rmEdge( g1.last_edge );
+	BOOST_CHECK_EQUAL( g1.last_node->data, 1);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
+	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
+	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
+	BOOST_CHECK_EQUAL( g1.last_node->next, null);
+	BOOST_CHECK_EQUAL( g1.last_node->prev, g1.first_node);
+	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, null);
+	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, null);
 	BOOST_CHECK_EQUAL( g1.first_edge, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.first_edge->next, null);
 	BOOST_CHECK_EQUAL( g1.first_edge->prev, null);
 
 	g1.addEdge( g1.last_node, g1.first_node );
-	BOOST_CHECK_EQUAL( g1.first_node->data, 1);
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
+	BOOST_CHECK_EQUAL( g1.last_node->data, 1);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
 	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
 	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
 	BOOST_CHECK_EQUAL( g1.last_node->next, null);
 	BOOST_CHECK_EQUAL( g1.last_node->prev, g1.first_node);
-	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, g1.last_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.last_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.first_edge->next, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.last_edge->prev, g1.first_edge);
 	BOOST_CHECK_EQUAL( g1.first_edge->prev, null);
 
-	g1.rmEdge( g1.last_edge );
-	BOOST_CHECK_EQUAL( g1.first_node->data, 1);
-	BOOST_CHECK_EQUAL( g1.last_node->data, 0);
+	g1.rmEdge( g1.first_edge );
+	BOOST_CHECK_EQUAL( g1.last_node->data, 1);
+	BOOST_CHECK_EQUAL( g1.first_node->data, 0);
 	BOOST_CHECK_EQUAL( g1.first_node->prev, null);
 	BOOST_CHECK_EQUAL( g1.first_node->next, g1.last_node);
 	BOOST_CHECK_EQUAL( g1.last_node->next, null);
 	BOOST_CHECK_EQUAL( g1.last_node->prev, g1.first_node);
 	BOOST_CHECK_EQUAL( g1.first_node->first_succ_edge, null);
-	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, g1.first_edge);
-	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.first_edge);
+	BOOST_CHECK_EQUAL( g1.first_node->first_pred_edge, g1.last_edge);
+	BOOST_CHECK_EQUAL( g1.last_node->first_succ_edge, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.last_node->first_pred_edge, null);
 	BOOST_CHECK_EQUAL( g1.first_edge, g1.last_edge);
 	BOOST_CHECK_EQUAL( g1.first_edge->next, null);
