b940df8cd45c0620aa68b0833b9dda82f112f496
Artem.Rudoj@frtk.ru <Artem.Rudoj@frtk.ru@8902bacc-9add-f0e9-d473-1da1e7f672c7>
2014-05-17 11:08:52 +0000

    fixed bugs

---
+++ trunk/sources/IR/mdes_dummy.cpp
@@ -0,0 +1,41 @@
+#include "ir_iface.hpp"
+
+/** Initialization of the object **/
+OperDes::OperDes( const char *name, int num_args, int num_res)
+{
+	int length;
+	length = strlen( name) + 1;
+	this->name = new char[length];
+	strcpy( this->name, name);
+	this->num_args = num_args;
+	this->num_res = num_res;
+};
+ 
+OperDes* MDES::getOperDes( OperName name)
+{
+	return &des[name];
+};
+
+/** Return the number of arguments */
+int MDES::numArgsInOperName( OperName op)
+{
+	OperDes *des = MDES::getOperDes( op);
+	return des->num_args;
+};
+
+/** Return the number of results */
+int MDES::numRessInOperName( OperName op)
+{
+	OperDes *des = MDES::getOperDes( op);
+	return des->num_res;
+};
+
+/** Return the name of function */
+char* MDES::getOperNameString( OperName op)
+{
+	OperDes *des = MDES::getOperDes( op);
+	return des->name;
+};
+
+/** Initialization the array of objects **/
+OperDes MDES::des[OPERS_NUM] = { OperDes( "MOV", 2 , 1), OperDes( "ADD", 2, 1), OperDes( "SUB", 2, 1), OperDes( "MUL", 2, 1)}; 
